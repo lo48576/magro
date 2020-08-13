54441071d6e169c623316b3f61e1fa03cd21b9b1
YOSHIOKA Takuma <lo48576@hard-wi.red>
2020-08-13 14:35:37 +0900

    Add basic files

---
+++ magro-cli/src/main.rs
@@ -0,0 +1,3 @@
+//! Command to manage git repositories.
+
+fn main() {}
---
+++ magro/src/lib.rs
@@ -0,0 +1,5 @@
+//! magro: Manage git repositories.
+#![forbid(unsafe_code)]
+#![warn(missing_docs)]
+#![warn(rust_2018_idioms)]
+#![warn(clippy::missing_docs_in_private_items)]
