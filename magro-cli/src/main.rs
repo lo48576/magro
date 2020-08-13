81e2821c114dcd32b667c25d07fcec137e40c650
YOSHIOKA Takuma <lo48576@hard-wi.red>
2020-08-13 14:46:38 +0900

    Set up logger

--- magro-cli/src/main.rs
+++ magro-cli/src/main.rs
@@ -1,3 +1,18 @@
 //! Command to manage git repositories.
 
-fn main() {}
+fn main() {
+    init_logger();
+}
+
+/// Initialize logger.
+fn init_logger() {
+    /// Default log filter for debug build.
+    #[cfg(debug_assertions)]
+    const DEFAULT_LOG_FILTER: &str = "magro=debug";
+    /// Default log filter for release build.
+    #[cfg(not(debug_assertions))]
+    const DEFAULT_LOG_FILTER: &str = "magro=info";
+
+    env_logger::Builder::from_env(env_logger::Env::default().default_filter_or(DEFAULT_LOG_FILTER))
+        .init();
+}
