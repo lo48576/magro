c21e4fb6fe6e77b26dae5d5f1c87b0604264e658
YOSHIOKA Takuma <lo48576@hard-wi.red>
2020-08-18 01:07:04 +0900

    Use dedicated opaque error type for git operation

--- magro/src/vcs/git.rs
+++ magro/src/vcs/git.rs
@@ -2,12 +2,34 @@
 
 use std::{borrow::Cow, iter, path::Path};
 
-use git2::{Error as GitError, Repository, RepositoryOpenFlags};
+use git2::{Repository, RepositoryOpenFlags};
+use thiserror::Error as ThisError;
+
+/// Error for git-related operations.
+#[derive(Debug, ThisError)]
+#[error(transparent)]
+pub(super) struct Error(anyhow::Error);
+
+impl Error {
+    /// Creates a new error.
+    #[inline]
+    #[must_use]
+    fn new(e: impl Into<anyhow::Error>) -> Self {
+        Self(e.into())
+    }
+}
+
+impl From<git2::Error> for Error {
+    #[inline]
+    fn from(e: git2::Error) -> Self {
+        Self::new(e)
+    }
+}
 
 /// Returns the working directory for the given repository if available.
 ///
 /// Note that `.git` directory should be passed for normal repsoitory as `repo` parameter.
-pub(super) fn workdir(repo_path: &Path) -> Result<Option<Cow<'_, Path>>, GitError> {
+pub(super) fn workdir(repo_path: &Path) -> Result<Option<Cow<'_, Path>>, Error> {
     // NO_SEARCH: No need of extra traversal because we already have
     // candidate path of the git directory.
     // NO_DOTGIT: No need of appending `/.git` because we already have
