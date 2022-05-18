e73e9cc4268b10cb5b507094f5ddcdbaf34103f6
YOSHIOKA Takuma <lo48576@hard-wi.red>
2022-05-19 04:15:11 +0900

    Bump deps versions

--- magro/src/lock_fs.rs
+++ magro/src/lock_fs.rs
@@ -4,18 +4,18 @@ use std::fs::File;
 use std::io::{Read, Result, Write};
 use std::path::Path;
 
-use fd_lock::FdLock;
+use fd_lock::RwLock;
 
 /// Opens a file and returns it in a lockable form.
 #[inline]
-pub(crate) fn open<P: AsRef<Path>>(path: P) -> Result<FdLock<File>> {
-    File::open(path).map(FdLock::new)
+pub(crate) fn open<P: AsRef<Path>>(path: P) -> Result<RwLock<File>> {
+    File::open(path).map(RwLock::new)
 }
 
 /// Opens or creates a file and returns it in a lockable form.
 #[inline]
-pub(crate) fn create<P: AsRef<Path>>(path: P) -> Result<FdLock<File>> {
-    File::create(path).map(FdLock::new)
+pub(crate) fn create<P: AsRef<Path>>(path: P) -> Result<RwLock<File>> {
+    File::create(path).map(RwLock::new)
 }
 
 /// Reads the file exclusively from the given flie into a string.
@@ -23,12 +23,12 @@ pub(crate) fn create<P: AsRef<Path>>(path: P) -> Result<FdLock<File>> {
 /// During the read, the file is locked.
 pub(crate) fn read_to_string_from_lockable_file(
     path: &Path,
-    file: &mut FdLock<File>,
+    file: &mut RwLock<File>,
 ) -> Result<String> {
     let mut content = String::new();
     {
         log::trace!("Locking file {} for read", path.display());
-        let mut lock = file.lock()?;
+        let mut lock = file.write()?;
         log::trace!("Successfully locked file {} for read", path.display());
         lock.read_to_string(&mut content)?;
     }
@@ -68,7 +68,7 @@ fn write_impl(path: &Path, contents: &[u8]) -> Result<()> {
     let mut file = create(path)?;
     log::trace!("Locking file {} for write", path.display());
     {
-        let mut lock = file.lock()?;
+        let mut lock = file.write()?;
         log::trace!("Successfully locked file {} for write", path.display());
         lock.write_all(contents)?;
     }
